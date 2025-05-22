d6c646386e44afc5b2831459a47fdf1907a6fb7b
Nuzul Abatony <albtony21@gmail.com>
2025-05-23 00:27:33 +0700

    feat: implement jwt auth token middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router(); 
 
+const authenticateToken = require('../middleware/authenticateToken.middleware');
+const authorizeRole = require('../middleware/authorizeRole.middleware');
+
 const employeeRoutes = require('./employee.route');
 const customerRoutes = require('./customer.route');
 const authRoutes = require('./auth.route');
 const roleRoutes = require('./role.route');
 
-router.use('/employees', employeeRoutes);
-router.use('/customers', customerRoutes);
 router.use('/auth', authRoutes);
-router.use('/role', roleRoutes);
+router.use('/role', authenticateToken, roleRoutes);
+router.use('/employees', authenticateToken, employeeRoutes);
+router.use('/customers', authenticateToken, customerRoutes);
 
 module.exports = router;
