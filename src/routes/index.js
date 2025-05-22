152cd8fb90ff2e85b40cd34b78e32c575d6d9bbb
Nuzul Abatony <albtony21@gmail.com>
2025-05-22 21:41:42 +0700

    feat: mount auth and role

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const employeeRoutes = require('./employee.route');
 const customerRoutes = require('./customer.route');
+const authRoutes = require('./auth.route');
+const roleRoutes = require('./role.route');
 
 router.use('/employees', employeeRoutes);
 router.use('/customers', customerRoutes);
+router.use('/auth', authRoutes);
+router.use('/role', roleRoutes);
 
 module.exports = router;
